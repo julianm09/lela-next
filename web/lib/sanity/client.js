3cdf871b4bbccc1c0f6e54279dd6a3328c42b818
Julian Mayes <juliantmayes@gmail.com>
2021-09-25 20:34:18 -0700

    updates

--- web/lib/sanity/client.js
+++ web/lib/sanity/client.js
@@ -4,4 +4,5 @@ export const client = sanityClient({
   dataset: "production",
   //   token: 'sanity-auth-token', // or leave commented out to be anonymous user
   useCdn: false, // `false` if you want to ensure fresh data
+  revalidate:1
 });
